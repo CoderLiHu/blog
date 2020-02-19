a146173b22d093c84feda6386113fcf4dd20e69e
lihu <lihu3059@gmail.com>
2020-02-19 14:39:27 +0800

    皮肤ok

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
+    post_list = Post.objects.all().order_by('-create_time')
     return render(request,
                   'blog/index.html',
                   context={
                       'title': '李虎的博客首页',
-                      'welcome': '欢迎访问我的博客首页'
+                      'post_list': post_list
                   })
