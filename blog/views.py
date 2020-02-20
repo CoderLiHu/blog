807a8123b368a3e3d5ca90226fd6ad980e9b01cc
lihu <lihu3059@gmail.com>
2020-02-21 00:04:47 +0800

    归档,分类,标签视图函数ok

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category, Tag
 import markdown
 import re
 from django.utils.text import slugify
@@ -21,12 +21,31 @@ def index(request):
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     md = markdown.Markdown(extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      TocExtension(slugify=slugify),
-                                  ])
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        TocExtension(slugify=slugify),
+    ])
     post.text = md.convert(post.text)
 
     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
     post.toc = m.group(1) if m is not None else ''
     return render(request, 'blog/detail.html', {"post": post})
+
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(
+        created_time__year=year,
+        created_time__month=month).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def tag(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tag=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
