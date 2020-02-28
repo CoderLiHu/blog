2d5050c3d2e5d781b665b84c03cd26d43f385d7e
lihu <lihu3059@gmail.com>
2020-02-28 19:04:12 +0800

    添加白名单

--- lihu_blog/settings.py
+++ lihu_blog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '*-pl&kr!+(+#aut$9r2k2$1fc_s240*^pyu*c6t(_qhultrd05'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', 'lihu.site']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', 'www.lihu.site']
 
 # Application definition
 
